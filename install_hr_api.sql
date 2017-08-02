920028da998fa8c3bdee334f9a9db077fec59198
Robert Lockard <n4281k@gmail.com>
2017-08-02 14:03:30 -0400

    pkg_emp_DML
    stubbed pIns_Emp and pSel_emp_by_phone

--- install_hr_api.sql
+++ install_hr_api.sql
@@ -9,12 +9,28 @@ create or replace package pkgEmp_DML authid current_user as
 	-- insert a row
 	procedure pIns_emp(pEmpRec hr_decls.r.t_emp_row);
 	-- select a row based on phone number
-	procedure pSel_emp(pPhoneNumber varchar2,
-					   pEmpRec hr_decls.r.t_emp_row);
+	procedure pSel_emp_by_phone(pPhoneNumber varchar2,
+								pEmpRec hr_decls.r.t_emp_row);
 end pEmp_DML;
 /
 
--- note use_sel_emp_role was granted to hr_api with delegate option.
+-- note use_emp_role was granted to hr_api with delegate option.
 -- because of this, we can grant the role to the package. at this
--- time the use_sel_emp_role does not have any privileges.
+-- time the use_emp_role does not have any privileges.
 grant use_emp_role to package pkgEmp_DML;
+
+create or replace package body pkg_Emp_DML AS
+	-- insert a row
+	procedure pIns_emp(pEmpRec hr_decls.r.t_emp_row) IS
+	BEGIN
+		/* TO DO FILL IN THE LOGIC*/
+		NULL;
+	END pIns_emp;
+	-- select a row based on phone number
+	procedure pSel_emp_by_phone(pPhoneNumber varchar2,
+								pEmpRec hr_decls.r.t_emp_row) IS
+	BEGIN
+		/* TO DO FILL IN THE LOGIC*/
+		NULL;
+	END pSel_emp_by_phone;
+END pkg_Emp_DML;
